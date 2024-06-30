44fb5f59f8ea8366b66ab0699344afd41b58605a
hillad3 <adam.x.hill@gmail.com>
2024-06-30 15:07:25 -0400

    Updated module name

---
+++ moduleVolumeConversion.R
@@ -0,0 +1,258 @@
+
+modVolumeConversionUI <- function(id_, vol_conv_){
+  tagList(
+    br(),
+    selectInput(
+      inputId = NS(id_,"ingredient"),
+      label = "Ingredient",
+      choices = c("", vol_conv_$ingredient),
+      selected = "",
+      multiple = FALSE
+    ),
+    fluidRow(
+      accordion(
+        open = FALSE,
+        multiple = FALSE,
+        accordion_panel(
+          "Starting Volume Inputs",
+          fluidRow(
+            column(
+              width = 2,
+              numericInput(
+                inputId = NS(id_,"volume_amt"),
+                label = "Amount",
+                value = 0,
+                min = 0,
+                step = 1/8
+              )
+            ),
+            column(
+              width = 3,
+              selectInput(
+                inputId = NS(id_,"volume_uom"),
+                label = "Unit of Measure",
+                choices = c("","cup","tablespoon","teaspoon"),
+                selected = "cup",
+                multiple = FALSE
+              )
+            )
+          )
+        )
+      )
+    ),
+    fluidRow(
+      accordion(
+        open = FALSE,
+        multiple = FALSE,
+        accordion_panel(
+          "Target Weight Inputs",
+          fluidRow(
+            column(
+              width = 3,
+              selectInput(
+                inputId = NS(id_,"target_uom"),
+                label = "Unit of Measure",
+                choices = c("","ounces","grams"),
+                selected = "grams",
+                multiple = FALSE
+              )
+            )
+          )
+        )
+      )
+    ),
+    # tags$p(paste0(
+    #   "Wordle Answer list updated ",
+    #   max(dt_words_[!is.na(Date)]$Date)," (",
+    #   days_since_last_update_,
+    #   ifelse(days_since_last_update_<=1," day ago)."," days ago)."),
+    #   " If applicable, today's word will not be displayed to prevent spoilers.")
+    # ),
+    # br(),
+    # accordion(
+    #   open = FALSE,
+    #   multiple = FALSE,
+    #   accordion_panel(
+    #     "Expand Filters",
+    #     textInput(
+    #      inputId = NS(id,"regex_str"),
+    #      label = "By Letter (see RegEx tab for instructions):",
+    #      value=""
+    #     ),
+    #     dateRangeInput(
+    #       inputId = NS(id,"date_range"),
+    #       label = "By Date Range",
+    #       start = "2021-06-19",
+    #       end = max_date_
+    #     ),
+    #     selectInput(
+    #       inputId = NS(id,"year_filter"),
+    #       label = "By Specific Year",
+    #       choices = c("", years_),
+    #       selected = "",
+    #       multiple = FALSE
+    #     ),
+    #     shinyWidgets::materialSwitch(
+    #       inputId = NS(id,"check_dups"),
+    #       label = "Check for Duplicates",
+    #       value = FALSE,
+    #       inline = TRUE
+    #     ),
+    #     if(!dups_present_){tags$p(paste0("No repeats identified as of ",max(dt_words_[!is.na(Date)]$Date)), style = "font-size:90%")},
+    #   )
+    # )
+  )
+}
+
+modVolumeConversionServer <- function(id_){
+  moduleServer(
+    id,
+    function(input, output, session){
+
+      re <- reactive(
+        if(is.null(input$regex_str) | input$regex_str==""){
+          "."
+        } else {
+          str_to_upper(input$regex_str)
+        }
+      )
+
+      dt <- reactive({
+
+        # logger::log_info(re())
+
+        dt <- tryCatch(
+          expr = {dt_words_[str_detect(Word, re())]},
+          error = function(e){dt_words_[Word=="12345"]}
+        )
+
+        if(!input$include_scrabble_dict){
+          dt <- dt[(Date >= input$date_range[1] & Date <= input$date_range[2])]
+        } else {
+          dt <- dt[is.na(Date) | (Date >= input$date_range[1] & Date <= input$date_range[2])]
+        }
+
+        if(input$include_scrabble_dict){
+          dt
+        } else {
+          dt <- dt[!is.na(Date)]
+          setorder(dt, -Index)
+        }
+
+        if(input$check_dups){
+          dt <- dt[duplicated(Word)]
+        }
+
+        dt
+
+      })
+
+      output$word_list_table <- renderDT(
+
+        if(dim(dt())[1]==0L){
+          data.table(" " = "There are no historical answers with the filtered parameters!")
+        } else {
+
+          if("Date" %in% names(dt())){
+            setnames(dt(), old = c("Date","Index"), new=c("Answer Date (YYYY-MM-DD)","Wordle Index"))
+          }
+
+          DT::datatable(
+            dt(),
+            options = list(
+              autoWidth=TRUE,
+              pageLength=10,
+              language = list(search = 'Normal Word Search:')
+            )) |>
+            formatStyle(columns = names(dt())[1], color = "#EDC001") |>
+            formatStyle(columns = names(dt())[2], color = "#3BC143")
+        }
+
+      )
+
+      output$letter_freq <- renderPlotly({
+        letter_counts <- dt() |>
+          unnest_tokens(char,Word,"characters") |>
+          mutate(
+            char = toupper(char),
+            position = forcats::fct(
+              rep(c("1st","2nd","3rd","4th","5th"), dim(dt())[1]),
+              c("5th","4th","3rd","2nd","1st")
+            )
+          ) |>
+          group_by(char, position) |>
+          reframe(n = n())
+
+        if(dim(letter_counts)[1]>0){
+          plot_ly(letter_counts, x = ~char, y = ~n, color = ~position, type = "bar") |>
+            layout(
+              barmode = "stack",
+              yaxis = list(title = "Frequency of Occurance", color = "#CCCCCC"),
+              xaxis = list(title = "Letters", color = "#CCCCCC"),
+              legend = list(title = list(text="<b>Letter Position</b>", font = list(color="#CCCCCC")),
+                            font = list(color="#CCCCCC")),
+              paper_bgcolor = "#363636",
+              plot_bgcolor = "#363636"
+            )
+        }
+
+      })
+
+
+      observeEvent(
+        input$year_filter,
+        if(input$year_filter==""){
+          updateDateRangeInput(
+            inputId = "date_range",
+            start = "2021-06-19",
+            end = max_date_
+          )
+        } else {
+          updateDateRangeInput(
+            inputId = "date_range",
+            start = paste0(input$year_filter,"-01-01"),
+            end = paste0(input$year_filter,"-12-31")
+          )
+        }
+      )
+
+      observeEvent(
+        input$reset_filter,
+        handlerExpr = {
+
+          updateTextInput(
+            session,
+            inputId = "regex_str",
+            value = ""
+          )
+
+          updateDateRangeInput(
+            session,
+            inputId = "date_range",
+            start = "2021-06-19",
+            end = max_date_
+          )
+
+          updateSelectInput(
+            session,
+            inputId = "year_filter",
+            selected = ""
+          )
+
+          updateMaterialSwitch(
+            session,
+            inputId = "include_scrabble_dict",
+            value = FALSE
+          )
+
+          updateMaterialSwitch(
+            session,
+            inputId = "check_dups",
+            value = FALSE
+          )
+        }
+      )
+
+    }
+  )
+}
